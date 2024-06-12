be439481a9eb7201eed0176a36d4084248bfb19a
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-06-12 22:16:38 +0200

    feat: change to json

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono! How are you? This is v2!");
+  return c.json({ message: "Hello Hono! How are you? This is v2!" });
 });
 
 export default app;
